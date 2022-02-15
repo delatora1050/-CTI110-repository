472a636e5b336480f932fdb827416409e56a46b2
delatora1050 <99104936+delatora1050@users.noreply.github.com>
2022-02-14 20:43:17 -0500

    Add files via upload
    Basic Math

---
+++ P1HW1_DeLaTorreAlestacia.py
@@ -0,0 +1,16 @@
+
+print("Enter the first number\n")
+user_num1 = int(input())
+print("Enter the second number\n")
+user_num2 = int(input())
+
+
+sum_num = user_num1 + user_num2
+
+
+product_num = user_num1 * user_num2
+
+print(user_num1, end = '\n')
+print(user_num2, end = '\n')
+print(sum_num, end = '\n')
+print(product_num, end = '\n')
